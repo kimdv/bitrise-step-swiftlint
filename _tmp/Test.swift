f8a649a87d67448d233644326187416488e0d061
Kim de Vos <kimdevos12@hotmail.com>
2017-09-07 22:40:55 +0200

    Made some changes in the test

---
+++ _tmp/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  SwiftLint
+//
+//  Created by Kim de Vos on 8/19/17.
+//  Copyright © 2017 Realm. All rights reserved.
+//
+
+import Foundation
+
+extension Test {
+
+    func foo() {  // Wrong
+        let date = Date() //    Wrong first on 25th place
+    }
+}
