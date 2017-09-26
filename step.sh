cbff45c5ee6e6a0949b7a6a349360b770ebae27d
Kim de Vos <kimdevos12@hotmail.com>
2017-09-26 18:14:35 +0200

    Added inputs to steps.sh

--- step.sh
+++ step.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -ex
 
-swiftlint lint
+if [ -z "${linting_path}" ] ; then
+  echo " [!] Missing required input: linting_path"
+  exit 1
+fi
+
+if [ -z "${lint_config_file}" ] ; then
+  echo " [!] Missing required input: lint_config_file"
+  exit 1
+fi
+
+cd "${linting_path}"
+
+swiftlint lint --path "${lint_config_file}"
