5cc489397609b0123e4a87b018ab51bff5a4ce59
Sam Dean <deanWombourne@gmail.com>
2017-11-17 19:55:49 +0000

    Add strict flag to the swiftlint step

--- step.sh
+++ step.sh
@@ -3,14 +3,20 @@ set -ex
 
 if [ -z "${linting_path}" ] ; then
   echo " [!] Missing required input: linting_path"
-  
+
   exit 1
+fi
+
+if [ "${strict}" = "no" ] ; then
+  STRICT_FLAG=
+else
+  STRICT_FLAG='--strict'
 fi
 
 cd "${linting_path}"
 
 if [ -z "${lint_config_file}" ] ; then
-  	swiftlint lint --reporter "${reporter}"
+  	swiftlint lint --reporter "${reporter}" ${STRICT_FLAG}
 else
-	swiftlint lint --config "${lint_config_file}" --reporter "${reporter}"
+	swiftlint lint --config "${lint_config_file}" --reporter "${reporter}" ${STRICT_FLAG}
 fi
