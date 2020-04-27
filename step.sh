29e89fc4735743ed58e401459a794fc9141227f3
Kim de Vos <kimdevos12@hotmail.com>
2020-04-27 19:33:19 +0200

    Made some simplifications
    Redirected the `stdout` from SwiftLint based on https://github.com/realm/SwiftLint/issues/2977

--- step.sh
+++ step.sh
@@ -34,10 +34,6 @@ case $lint_range in
   git diff --cached --name-only | while read filename; do run_swiftlint "${filename}" "${FLAGS}"; done 
   ;;
 "all")
-  output="$(swiftlint lint --reporter "${reporter}" ${FLAGS})"
-  envman add --key "SWIFTLINT_REPORT" --value "${output}"
-  echo "Saved swiftlint output in SWIFTLINT_REPORT"
-
   filename="swiftlint_report"
   case $reporter in
       xcode|emoji)
@@ -58,8 +54,8 @@ case $lint_range in
   esac
 
   report_path="${BITRISE_DEPLOY_DIR}/${filename}"
-  echo "${output}" > $report_path
-  envman add --key "SWIFTLINT_REPORT_PATH" --value "${report_path}"
-  echo "Saved swiftlint output in file, it's path is saved in SWIFTLINT_REPORT_PATH"
+
+  echo "Saved swiftlint output in file, it's path is saved in ${report_path}"
+  swiftlint lint --reporter "${reporter}" ${FLAGS} > $report_path
   ;;
 esac
